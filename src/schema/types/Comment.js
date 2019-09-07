6baf633f2dc3e1be98ca904d1f79041f81c8a60c
David Kananda <davidkananda@gmail.com>
2019-09-07 23:18:03 +0100

    add input types

--- src/schema/types/Comment.js
+++ src/schema/types/Comment.js
@@ -1,11 +1,28 @@
-import { GraphQLObjectType, GraphQLString, GraphQLInt } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInt
+} from 'graphql';
 
-const export Comment = new GraphQLObjectType({
-	name: "Comment",
-	description: "Details of a comment",
-	fields: () => ({
-		id: { type: GraphQLInt },
-		name: { type: GraphQLString },
-		content: { type: GraphQLString }
-	})
+export const Comment = new GraphQLObjectType({
+  name: 'Comment',
+  description: 'Details of a comment',
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    content: { type: GraphQLString }
+  })
+});
+
+export const CommentInputType = new GraphQLInputObjectType({
+  name: 'CommentInput',
+  fields: {
+    title: {
+      name: new GraphQLNonNull(GraphQLString),
+      content: new GraphQLNonNull(GraphQLString),
+      postId: new GraphQLNonNull(GraphQLInt)
+    }
+  }
 });
--- src/schema/types/Post.js
+++ src/schema/types/Post.js
@@ -1,26 +1,44 @@
-import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } from "graphql";
-import { Author } from "./Author";
-import { Comment } from "./Comment";
-import { fakeDatabase } from "../../FakeDatabase";
+import {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
+} from 'graphql';
+import { Author } from './Author';
+import { Comment } from './Comment';
+import { fakeDatabase } from '../../FakeDatabase';
 
-const export Post = new GraphQLObjectType({
-	name: "Post",
-	description: "All details of a blog post",
-	fields: () => ({
-		id: { type: GraphQLInt },
-		title: { type: GraphQLString },
-		content: { type: GraphQLString },
-		author: {
-			type: Author,
-			resolve: (post) => {
-				return fakeDatabase.getAuthor(post.author);
-			}
-		},
-		comments: {
-			type: new GraphQLList(Comment),
-			resolve: (post) => {
-				return fakeDatabase.getCommentsForPost(post.id);
-			}
-		}
-	})
+export const Post = new GraphQLObjectType({
+  name: 'Post',
+  description: 'All details of a blog post',
+  fields: () => ({
+    id: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    author: {
+      type: Author,
+      resolve: post => {
+        return fakeDatabase.getAuthor(post.author);
+      }
+    },
+    comments: {
+      type: new GraphQLList(Comment),
+      resolve: post => {
+        return fakeDatabase.getCommentsForPost(post.id);
+      }
+    }
+  })
+});
+
+export const PostInputType = new GraphQLInputObjectType({
+  name: 'PostInput',
+  fields: {
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+      content: new GraphQLNonNull(GraphQLString),
+      content: new GraphQLNonNull(GraphQLString)
+    }
+  }
 });
