bddc5ed9845badb6d7826f3259c2265ac9fcf7eb
David Kananda <davidkananda@gmail.com>
2019-09-08 00:38:51 +0100

    add graphiql and express server

---
+++ src/serve.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+console.log('Starting the server...');
+app.listen(4000);
